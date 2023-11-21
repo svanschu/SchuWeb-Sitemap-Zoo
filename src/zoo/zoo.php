3c8074dbaddfcfa5bbe66fa6807677f9d6978ae5
Sven Schultschik <sven@schultschik.de>
2023-11-21 19:34:45 +0100

    Adapt structure and logic to fit SchuWeb Sitemap v5
    Signed-off-by: Sven Schultschik <sven@schultschik.de>

--- src/zoo/zoo.php
+++ src/zoo/zoo.php
@@ -1,238 +1,221 @@
 <?php
 /**
- * @version             sw.build.version
- * @copyright (C)       2010 - 2022 Sven Schultschik. All rights reserved
- * @author              Sven Schultschik
- * @license             GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- * @link                http://www.schultschik.de
+ * @package     SchuWeb_Sitemap
+ * @subpackage  SchuWeb_Sitemap_Zoo
+ * 
+ * @version     sw.build.version
+ * @author      Sven Schultschik
+ * @copyright   (C) 2010 - 2023 Sven Schultschik. All rights reserved
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * @link        http://extensions.schultschik.de
  */
 defined('_JEXEC') or die;
 
-use \Joomla\Utilities\ArrayHelper;
+use Joomla\Utilities\ArrayHelper;
+use Joomla\CMS\Factory;
 
 class schuweb_sitemap_zoo
 {
+    protected static $_menu_items;
 
-	protected static $_menu_items;
+    static function prepareMenuItem(&$node, &$params)
+    {
+        $link_query = parse_url($node->link);
+        parse_str(html_entity_decode($link_query['query']), $link_vars);
+        $component = ArrayHelper::getValue($link_vars, 'option', '');
+        $view      = ArrayHelper::getValue($link_vars, 'view', '');
 
-	static function prepareMenuItem(&$node)
-	{
-		$link_query = parse_url($node->link);
-		parse_str(html_entity_decode($link_query['query']), $link_vars);
-		$component = ArrayHelper::getValue($link_vars, 'option', '');
-		$view      = ArrayHelper::getValue($link_vars, 'view', '');
+        if ($component == 'com_zoo' && $view == 'frontpage') {
+            $id = intval(ArrayHelper::getValue($link_vars, 'id', 0));
+            if ($id != 0) {
+                $node->uid        = 'zoo' . $id;
+                $node->expandible = false;
+            }
+        }
+    }
 
-		if ($component == 'schuweb_sitemap_zoo' && $view == 'frontpage')
-		{
-			$id = intval(ArrayHelper::getValue($link_vars, 'id', 0));
-			if ($id != 0)
-			{
-				$node->uid        = 'zoo' . $id;
-				$node->expandible = false;
-			}
-		}
-	}
+    static function getTree(&$sitemap, &$parent, &$params)
+    {
 
-	static function getTree(&$schuweb_sitemap, &$parent, &$params)
-	{
+        $link_query = parse_url($parent->link);
+        parse_str(html_entity_decode($link_query['query']), $link_vars);
 
-		$link_query = parse_url($parent->link);
-		parse_str(html_entity_decode($link_query['query']), $link_vars);
+        $include_categories           = ArrayHelper::getValue($params, 'include_categories', 1);
+        $include_categories           = ($include_categories == 1
+            || ($include_categories == 2 && $sitemap->isXmlsitemap())
+            || ($include_categories == 3 && !$sitemap->isXmlsitemap()));
+        $params['include_categories'] = $include_categories;
 
-		$include_categories           = ArrayHelper::getValue($params, 'include_categories', 1);
-		$include_categories           = ($include_categories == 1
-			|| ($include_categories == 2 && $schuweb_sitemap->view == 'xml')
-			|| ($include_categories == 3 && $schuweb_sitemap->view == 'html'));
-		$params['include_categories'] = $include_categories;
+        $include_items           = ArrayHelper::getValue($params, 'include_items', 1);
+        $include_items           = ($include_items == 1
+            || ($include_items == 2 && $sitemap->isXmlsitemap())
+            || ($include_items == 3 && !$sitemap->isXmlsitemap()));
+        $params['include_items'] = $include_items;
 
-		$include_items           = ArrayHelper::getValue($params, 'include_items', 1);
-		$include_items           = ($include_items == 1
-			|| ($include_items == 2 && $schuweb_sitemap->view == 'xml')
-			|| ($include_items == 3 && $schuweb_sitemap->view == 'html'));
-		$params['include_items'] = $include_items;
+        $priority   = ArrayHelper::getValue($params, 'cat_priority', $parent->priority);
+        $changefreq = ArrayHelper::getValue($params, 'cat_changefreq', $parent->changefreq);
+        if ($priority == '-1')
+            $priority = $parent->priority;
+        if ($changefreq == '-1')
+            $changefreq = $parent->changefreq;
 
-		$priority   = ArrayHelper::getValue($params, 'cat_priority', $parent->priority);
-		$changefreq = ArrayHelper::getValue($params, 'cat_changefreq', $parent->changefreq);
-		if ($priority == '-1')
-			$priority = $parent->priority;
-		if ($changefreq == '-1')
-			$changefreq = $parent->changefreq;
+        $params['cat_priority']   = $priority;
+        $params['cat_changefreq'] = $changefreq;
 
-		$params['cat_priority']   = $priority;
-		$params['cat_changefreq'] = $changefreq;
+        $priority   = ArrayHelper::getValue($params, 'item_priority', $parent->priority);
+        $changefreq = ArrayHelper::getValue($params, 'item_changefreq', $parent->changefreq);
+        if ($priority == '-1')
+            $priority = $parent->priority;
 
-		$priority   = ArrayHelper::getValue($params, 'item_priority', $parent->priority);
-		$changefreq = ArrayHelper::getValue($params, 'item_changefreq', $parent->changefreq);
-		if ($priority == '-1')
-			$priority = $parent->priority;
+        if ($changefreq == '-1')
+            $changefreq = $parent->changefreq;
 
-		if ($changefreq == '-1')
-			$changefreq = $parent->changefreq;
+        $params['item_priority']   = $priority;
+        $params['item_changefreq'] = $changefreq;
 
-		$params['item_priority']   = $priority;
-		$params['item_changefreq'] = $changefreq;
+        self::getCategoryTree($sitemap, $parent, $params);
 
-		self::getCategoryTree($schuweb_sitemap, $parent, $params);
+    }
 
-	}
+    static function getCategoryTree(&$sitemap, &$parent, &$params)
+    {
+        $db = Factory::getDBO();
 
-	static function getCategoryTree(&$schuweb_sitemap, &$parent, &$params)
-	{
-		$db = JFactory::getDBO();
+        // first we fetch what application we are talking about
 
-		// first we fetch what application we are talking about
+        $app        = Factory::getApplication();
+        $menu       = $app->getMenu('site');
+        $menuparams = $menu->getParams($parent->id);
+        $appid      = intval($menuparams->get('application', 0));
 
-		$app        = JFactory::getApplication('site');
-		$menu       = $app->getMenu();
-		$menuparams = $menu->getParams($parent->id);
-		$appid      = intval($menuparams->get('application', 0));
+        // if selected, we print title category
+        if ($params['include_categories']) {
 
-		// if selected, we print title category
-		if ($params['include_categories'])
-		{
+            // we print title if there is any
+            // commented out as non-functioning - Matt Faulds
+            //	if ($params['categories_title'] != "" && !$sitemap->isXmlsitemap()) {
+            //		echo "<".$params['categories_title_tag'].">".$params['categories_title']."</".$params['categories_title_tag'].">";
+            //	}
+            // get categories info from database
 
-			// we print title if there is any
-			// commented out as non-functioning - Matt Faulds
-			//	if ($params['categories_title'] != "" && $schuweb_sitemap->view == 'html') {
-			//		echo "<".$params['categories_title_tag'].">".$params['categories_title']."</".$params['categories_title_tag'].">";
-			//	}
-			// get categories info from database
-			$queryc = 'SELECT c.id, c.name ' .
-				'FROM #__zoo_category c ' .
-				' WHERE c.application_id = ' . $appid . ' AND c.published=1 ' .
-				' ORDER by c.ordering';
+            // $queryc = 'SELECT c.id, c.name ' .
+            //     'FROM #__zoo_category c ' .
+            //     ' WHERE c.application_id = ' . $appid . ' AND c.published=1 ' .
+            //     ' ORDER by c.ordering';
 
-			$db->setQuery($queryc);
-			$cats = $db->loadObjectList();
+            $query = $db->getQuery(true);
+            $query->select(
+                array(
+                    $db->qn('id'),
+                    $db->qn('name')
+                )
+            )
+                ->from($db->qn('#__zoo_category'))
+                ->where(
+                    array(
+                        $db->qn('application_id') . '=' . $db->q($appid),
+                        $db->qn('published') . '=1'
+                    )
+                )
+                ->order($db->escape('ordering'));
 
-			// now we print categories
-			$schuweb_sitemap->changeLevel(1);
-			foreach ($cats as $cat)
-			{
+            $db->setQuery($query);
+            $cats = $db->loadObjectList();
 
-				// Added by Matt Faulds to allow SEF urls
-				if (!($Itemid = self::_find('frontpage', $appid)->id))
-				{
-					$Itemid = self::_find('category', $appid)->id;
-				}
+            foreach ($cats as $cat) {
+                $node             = new stdclass;
+                $node->id         = $parent->id;
+                $id               = $node->uid = $parent->uid . 'c' . $cat->id;
+                $node->browserNav = $parent->browserNav;
+                $node->name       = $cat->name;
+                $node->link       = 'index.php?option=com_zoo&amp;task=category&amp;category_id='
+                    . $cat->id . '&amp;Itemid=' . $node->id;
+                $node->priority   = $params['cat_priority'];
+                $node->changefreq = $params['cat_changefreq'];
 
-				$node             = new stdclass;
-				$node->id         = $parent->id;
-				$node->uid        = $parent->uid . 'c' . $cat->id;
-				$node->name       = $cat->name;
-				$node->link       = 'index.php?option=schuweb_sitemap_zoo&amp;task=category&amp;category_id=' . $cat->id . '&amp;Itemid=' . $Itemid;
-				$node->priority   = $params['cat_priority'];
-				$node->changefreq = $params['cat_changefreq'];
+                $node->xmlInsertChangeFreq = $parent->xmlInsertChangeFreq;
+                $node->xmlInsertPriority   = $parent->xmlInsertPriority;
 
-				$attribs                   = json_decode($schuweb_sitemap->sitemap->attribs);
-				$node->xmlInsertChangeFreq = $attribs->xmlInsertChangeFreq;
-				$node->xmlInsertPriority   = $attribs->xmlInsertPriority;
+                $node->expandible = true;
+                $node->lastmod    = $parent->lastmod;
 
-				$node->expandible = true;
-				$node->lastmod    = $parent->lastmod;
-				$schuweb_sitemap->printNode($node);
-			}
-			$schuweb_sitemap->changeLevel(-1);
-		}
+                if (!isset($parent->subnodes))
+                    $parent->subnodes = new \stdClass();
 
-		if ($params['include_items'])
-		{
-			// get items info from database
-			// basically it select those items that are published now (publish_up is less then now, meaning it's in past)
-			// and not unpublished yet (either not have publish_down date set, or that date is in future)
-			$queryi = 'SELECT i.id, i.name, i.publish_up ,i.application_id ,i.modified' .
-				' FROM #__zoo_item i' .
-				' WHERE i.application_id= ' . $appid .
-				' AND DATEDIFF( i.publish_up, NOW( ) ) <=0' .
-				' AND IF( i.publish_down >0, DATEDIFF( i.publish_down, NOW( ) ) >0, true )' .
-				' ORDER BY i.publish_up';
-			$db->setQuery($queryi);
-			$items = $db->loadObjectList();
+                $node->params = &$parent->params;
 
-			// now we print items
-			$schuweb_sitemap->changeLevel(1);
-			foreach ($items as $item)
-			{
-				if (is_null($menuitem = self::_find('frontpage', $appid)))
-				{
-					if (is_null($menuitem = self::_find('category', $appid)))
-					{
-						$menuitem = self::_find('item', $item->id);
-					}
-				}
-				$Itemid = $menuitem->id;
+                $parent->subnodes->$id = $node;
+            }
+        }
 
-				// if we are making news map, we should ignore items older then 3 days
-				if ($schuweb_sitemap->isNews && strtotime($item->publish_up) < ($schuweb_sitemap->now - (3 * 86400)))
-				{
-					continue;
-				}
-				$node             = new stdclass;
-				$node->id         = $parent->id;
-				$node->uid        = $parent->uid . 'i' . $item->id;
-				$node->name       = $item->name;
-				$node->link       = 'index.php?option=schuweb_sitemap_zoo&amp;task=item&amp;item_id=' . $item->id . '&amp;Itemid=' . $Itemid;
-				$node->priority   = $params['item_priority'];
-				$node->changefreq = $params['item_changefreq'];
+        if ($params['include_items']) {
+            // get items info from database
+            // basically it select those items that are published now (publish_up is less then now, meaning it's in past)
+            // and not unpublished yet (either not have publish_down date set, or that date is in future)
+            
+            // $queryi = 'SELECT i.id, i.name, i.publish_up ,i.application_id ,i.modified' .
+            //     ' FROM #__zoo_item i' .
+            //     ' WHERE i.application_id= ' . $appid .
+            //     ' AND DATEDIFF( i.publish_up, NOW( ) ) <=0' .
+            //     ' AND IF( i.publish_down >0, DATEDIFF( i.publish_down, NOW( ) ) >0, true )' .
+            //     ' ORDER BY i.publish_up';
 
-				$attribs                   = json_decode($schuweb_sitemap->sitemap->attribs);
-				$node->xmlInsertChangeFreq = $attribs->xmlInsertChangeFreq;
-				$node->xmlInsertPriority   = $attribs->xmlInsertPriority;
+            $query = $db->getQuery(true);
+            $query->select(
+                array(
+                    $db->qn('id'),
+                    $db->qn('name'),
+                    $db->qn('publish_up'),
+                    $db->qn('application_id'),
+                    $db->qn('modified')
+                ))
+                ->from($db->qn('#__zoo_item'))
+                ->where(
+                    array(
+                        $db->qn('application_id') . '=' . $db->q($appid),
+                        'DATEDIFF(' . $db->qn('publish_up') . ' , NOW()) <=0',
+                        'IF(' . $db->qn('publish_down') . ' >0, DATEDIFF( ' . $db->qn('publish_down') . ', NOW( ) ) >0, true )'
+                    ))
+                ->order($db->qn('publish_up'));
 
-				$node->expandible = true;
-				$node->lastmod    = $parent->lastmod;
-				$node->modified   = strtotime($item->modified);
-				$node->newsItem   = 1; // if we are making news map and it get this far, it's news
-				$schuweb_sitemap->printNode($node);
 
-			}
-			$schuweb_sitemap->changeLevel(-1);
-		}
-	}
+            $db->setQuery($query);
+            $items = $db->loadObjectList();
 
-	// Adapted from ZOO 2.5.10
-	// Added by Matt Faulds to allow SEF urls
-	static protected function _find($type, $id)
-	{
+            // now we print items
+            foreach ($items as $item) {
+                // if we are making news map, we should ignore items older then 3 days
+                if (
+                    $sitemap->isNews
+                    && strtotime($item->publish_up) < ($sitemap->now - (3 * 86400))
+                ) {
+                    continue;
+                }
+                $node             = new stdclass;
+                $node->id         = $parent->id;
+                $id               = $node->uid = $parent->uid . 'i' . $item->id;
+                $node->browserNav = $parent->browserNav;
+                $node->name       = $item->name;
+                $node->link       = 'index.php?option=com_zoo&amp;task=item&amp;item_id='
+                    . $item->id . '&amp;Itemid=' . $parent->id;
+                $node->priority   = $params['item_priority'];
+                $node->changefreq = $params['item_changefreq'];
 
-		// load config
-		require_once(JPATH_ADMINISTRATOR . '/components/schuweb_sitemap_zoo/config.php');
+                $node->xmlInsertChangeFreq = $parent->xmlInsertChangeFreq;
+                $node->xmlInsertPriority   = $parent->xmlInsertPriority;
 
-		// get ZOO app
-		$app = App::getInstance('zoo');
+                $node->expandible = true;
+                $node->lastmod    = $parent->lastmod;
+                $node->modified   = strtotime($item->modified);
+                $node->newsItem   = 1; // if we are making news map and it get this far, it's news
 
-		if (self::$_menu_items == null)
-		{
-			$menu_items = $app->object->create('\Joomla\CMS\Application\SiteApplication')->getMenu()->getItems('component_id', JComponentHelper::getComponent('schuweb_sitemap_zoo')->id);
+                if (!isset($parent->subnodes))
+                    $parent->subnodes = new \stdClass();
 
-			$menu_items = $menu_items ?: array();
+                $node->params = &$parent->params;
 
-			self::$_menu_items = array_fill_keys(array('category', 'frontpage', 'item'), array());
-			foreach ($menu_items as $menu_item)
-			{
-				switch (@$menu_item->query['view'])
-				{
-					case 'frontpage':
-						self::$_menu_items['frontpage'][$app->parameter->create($menu_item->getParams())->get('application')] = $menu_item;
-						break;
-					case 'category':
-						self::$_menu_items['category'][$app->parameter->create($menu_item->getParams())->get('category')] = $menu_item;
-						break;
-					case 'item':
-						self::$_menu_items['item'][$app->parameter->create($menu_item->getParams())->get('item_id')] = $menu_item;
-						break;
-				}
-			}
-		}
-
-		if (array_key_exists($type, self::$_menu_items) && array_key_exists($id, self::$_menu_items[$type]))
-		{
-			return self::$_menu_items[$type][$id];
-		}
-		else
-		{
-			return null;
-		}
-	}
-
+                $parent->subnodes->$id = $node;
+            }
+        }
+    }
 }
